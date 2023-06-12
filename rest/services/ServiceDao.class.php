2e885b1dacbf5526d1b85e146a162365c7a834b4
ishak-dev <ishak.kazic@gmail.com>
2023-06-12 23:48:59 +0200

    Update ServiceDao.class.php

--- rest/services/ServiceDao.class.php
+++ rest/services/ServiceDao.class.php
@@ -15,5 +15,12 @@ class ServiceDao extends BaseDao
   public function select_by_user_id($id){
     return $this->query("SELECT * FROM services WHERE VehicleID = :id",["id"=>$id]);
   }
+  public function getCarOwner($id){
+    $stmt = $this->connection->prepare("SELECT * FROM vehicles WHERE CustomerId = :id");
+    $stmt->bindParam(":id",$id);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result['id'];
+  }
 }
 ?>
