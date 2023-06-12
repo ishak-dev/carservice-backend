06fde096d03da6785ff740d55ee90e512830e16d
ishak-dev <ishak.kazic@gmail.com>
2023-06-12 23:48:55 +0200

    Update ServiceService.class.php

--- rest/services/ServiceService.class.php
+++ rest/services/ServiceService.class.php
@@ -9,8 +9,15 @@ class ServiceService extends BaseService{
   public function __construct(){
     parent::__construct(new ServiceDao());
   }
-  public function select_by_user_id($id){
-    return $this->dao->select_by_user_id($id);
+  public function select_by_user_id(){
+    $carId = $this->dao->getCarOwner(Flight::get("user")["id"]);
+    $data["VehicleId"] = $carId;
+    return $this->dao->select_by_user_id($carId);
+  }
+  public function addService($data){
+    $carId = $this->dao->getCarOwner(Flight::get("user")["id"]);
+    $data["VehicleId"] = $carId;
+    return $this->dao->add($data);
   }
 }
 ?>
